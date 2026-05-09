8941b9e317b36af779a160647e8f985e12315b14
runlingli <lirunlingemily@gmail.com>
2026-05-09 15:25:01 -0700

    feature: add prototype

---
+++ reHome/reHome/reHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  reHomeApp.swift
+//  reHome
+//
+//  Created by runling on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct reHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
